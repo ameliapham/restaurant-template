747fb8d7c1aaf101844c95a5ef50b4d5244d6fea
ameliapham <ptt.huong.8895@gmail.com>
2024-03-21 18:18:26 +0100

    Add other token color in palette theme, mode dark

--- src/theme.ts
+++ src/theme.ts
@@ -17,5 +17,25 @@ export const darkTheme = createTheme({
             light: alpha("#D4A373", 0.4),
             dark: alpha("#D4A373", 1.0),
         },
+        error: {
+            main: "#E04E41",
+            light: "#EF7F67",
+            dark: "#CC0B0B",
+        },
+        warning: {
+            main: "#FFB93F",
+            light: "#FFCD66",
+            dark: "#FF9800",
+        },
+        info: {
+            main: "#57B9F7",
+            light: "#79D0FB",
+            dark: "#2196F3",
+        },
+        success: {
+            main: "#94A868",
+            light: "#F5FDC6",
+            dark: "#41521F",
+        },
     },
 });
