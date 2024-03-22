205b117b66a1ed8d26299c5f393436348bf050d3
ameliapham <ptt.huong.8895@gmail.com>
2024-03-22 19:39:03 +0100

    Update AllTogether

--- src/pages/Home/AllTogether.tsx
+++ src/pages/Home/AllTogether.tsx
@@ -1,9 +1,33 @@
+import { tss } from 'tss-react/mui'
+
 import { LeftSide } from "./LeftSide"
+import { RightSide } from "./RightSide"
+
 
 export function AllTogether() {
+
+    const { cx, classes } = useStyles()
+
     return (
-        <>
-            <LeftSide />
-        </>
+        <div className={cx(classes.root)}>
+            <LeftSide className={classes.left}/>
+            <RightSide className={classes.right}/>
+        </div>
     )
 }
+
+const useStyles = tss.create(({ theme }) => ({
+    "root": {
+        "display": "flex",
+        "gap": "10px",
+        "padding": "10px",
+        "border": `1px solid ${theme.palette.primary.main}`,
+        "borderRadius": "20px",
+    },
+    "left": {
+        "width": "70%",
+    },
+    "right": {
+        "width": "30%",
+    }
+}));
