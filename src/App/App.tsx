a6be5e67254db6933251d2285d1e7b6a24d91f61
ameliapham <ptt.huong.8895@gmail.com>
2024-03-22 02:04:07 +0100

    Add GlobalStyles to App

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,3 +1,27 @@
+import { darkTheme } from "../theme.tsx"
+import { GlobalStyles } from 'tss-react';
+import { tss } from 'tss-react/mui';
+
+
 export function App() {
-  return <h1>Hello World</h1>
+  return (
+    <>
+      <GlobalStyles
+        styles={{
+          "html, body": {
+            margin: 0,
+            padding: 0,
+            backgroundColor: darkTheme.palette.background.default,
+            color: darkTheme.palette.text.primary,
+          },
+        }}
+      />
+
+
+      <div>
+        Hello World
+      </div>
+    </>
+
+  )
 }
