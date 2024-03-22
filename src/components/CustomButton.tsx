b5a09791ea6aaa73eb36698f040cc2621ee60feb
ameliapham <ptt.huong.8895@gmail.com>
2024-03-22 23:33:54 +0100

    Create CustomButton

---
+++ src/components/CustomButton.tsx
@@ -0,0 +1,43 @@
+import { tss } from 'tss-react/mui'
+import { alpha } from "@mui/material/styles";
+
+type PropsCustomButton = {
+    className?: string;
+    onClick?: () => void;
+    onHover: () => void;
+    children: React.ReactNode;
+}
+
+export function CustomButton(props: PropsCustomButton) {
+
+    const { className, onClick, onHover, children } = props
+    const { cx, classes } = useStyles()
+
+    return (
+        <button
+            className={cx(classes.button, className)}
+            onClick={onClick}
+            onMouseOver={onHover}
+        >
+            {children}
+        </button>
+    )
+}
+
+const useStyles = tss
+    .create(({ theme }) => ({
+        "button": {
+            "border": `1px solid ${theme.palette.primary.main}`,
+            "background": theme.palette.primary.main,
+            "color": theme.palette.primary.contrastText,
+            "padding": "10px",
+            "borderRadius": "5px",
+            "cursor": "pointer",
+            "transition": "background 0.4s ease-in-out, border 0.4s ease-in-out",
+            "&:hover": {
+                "background": alpha(theme.palette.secondary.light, 0.15),
+                "border": `1px solid ${alpha(theme.palette.secondary.light, 0.2)}`,
+
+            }
+        }
+    }))
