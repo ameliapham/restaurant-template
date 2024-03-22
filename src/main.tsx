30bcd4c04b7da48a8f3c63d46c1029c2103c6e85
ameliapham <ptt.huong.8895@gmail.com>
2024-03-22 01:40:08 +0100

    Update main with theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App.tsx'
+import { ThemeProvider } from '@mui/material/styles'
+import { darkTheme } from './theme.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <ThemeProvider theme={darkTheme}>
     <App />
-  </React.StrictMode>,
+  </ThemeProvider>,
 )
