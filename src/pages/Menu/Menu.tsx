f1595b7ffbc9fb06bde62099e984a25823f13f9a
ameliapham <ptt.huong.8895@gmail.com>
2024-03-25 20:03:25 +0100

    Create page Menu

---
+++ src/pages/Menu/Menu.tsx
@@ -0,0 +1,33 @@
+import { tss } from 'tss-react/mui'
+
+import { RightSide } from "./RightSide"
+import { LeftSide } from "./LeftSide"
+
+
+export function Menu() {
+
+    const { cx, classes } = useStyles()
+
+    return (
+        <div className={cx(classes.root)}>
+            <LeftSide className={classes.left}/>
+            <RightSide className={classes.right}/>
+        </div>
+    )
+}
+
+const useStyles = tss.create(({ theme }) => ({
+    "root": {
+        "display": "flex",
+        "flexGrow": 1,
+        "gap": "20px",
+        "borderRadius": "20px",
+        "margin": "30px",
+    },
+    "left": {
+        "width": "75%",
+    },
+    "right": {
+        "width": "25%",
+    }
+}));
