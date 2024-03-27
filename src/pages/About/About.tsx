061556d6fa9d33619efeae47ebd3dd4ffe7e5203
ameliapham <ptt.huong.8895@gmail.com>
2024-03-27 03:58:04 +0100

    Create page About

---
+++ src/pages/About/About.tsx
@@ -0,0 +1,44 @@
+import { tss } from 'tss-react/mui'
+
+import { RightSide } from "./RightSide"
+import { LeftSide } from "./LeftSide"
+
+type PropsAbout = {
+    className?: string;
+    onChangePage: (page: SelectedPage) => void;
+}
+
+type SelectedPage = "home" | "menu" | "about" | "book"
+
+export function About(props: PropsAbout) {
+
+    const { cx, classes } = useStyles()
+
+    const { onChangePage } = props
+
+    return (
+        <div className={cx(classes.root)}>
+            <LeftSide
+                className={classes.left}
+                onChangePage={onChangePage}
+            />
+            <RightSide className={classes.right} />
+        </div>
+    )
+}
+
+const useStyles = tss.create(({ theme }) => ({
+    "root": {
+        "display": "flex",
+        "flexGrow": 1,
+        "gap": "20px",
+        "borderRadius": "20px",
+        "margin": "30px",
+    },
+    "left": {
+        "width": "60%",
+    },
+    "right": {
+        "width": "40%",
+    }
+}));
