f2865d287766ac9727454181a9fad4dc018158c4
ameliapham <ptt.huong.8895@gmail.com>
2024-03-27 01:54:20 +0100

    Update CustomButton with selected and hoverEffect

--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -3,25 +3,24 @@ import { alpha } from "@mui/material/styles";
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import React from 'react';
 
-type PropsCustomButton = {
+type CustomButtonProps = {
     className?: string;
     onClick?: () => void;
-    onHover?: () => void;
     children: React.ReactNode;
     icon?: React.ReactNode | boolean;
+    selected: boolean;
 }
 
-export function CustomButton(props: PropsCustomButton) {
+export function CustomButton(props: CustomButtonProps) {
 
-    const { className, onClick, onHover, children, icon } = props
+    const { className, onClick, children, icon, selected } = props
     const { cx, classes } = useStyles()
     const Icon = React.isValidElement(icon) ? icon : icon ? <ArrowForwardIcon /> : null
 
     return (
         <button
-            className={cx(classes.button, className)}
+            className={cx(classes.button, {[classes.selected]: selected}, classes.hoverEffect, className)}
             onClick={onClick}
-            onMouseOver={onHover}
         >
             {children}
             {Icon}
@@ -42,10 +41,15 @@ const useStyles = tss
             "borderRadius": "5px",
             "cursor": "pointer",
             "transition": "background 0.4s ease-in-out, border 0.4s ease-in-out, color 0.4s ease-in-out",
-            "&:hover": {
+        },
+        "hoverEffect": {
+            "&:not($selected):hover": {
                 "background": alpha(theme.palette.secondary.light, 0.15),
                 "border": `1px solid ${alpha(theme.palette.secondary.light, 0.2)}`,
-
             }
+        },
+        "selected": {
+            "background": theme.palette.secondary.dark,
+            "color": theme.palette.primary.main,
         }
     }))
