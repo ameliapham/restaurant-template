c4f0f4597abfad0be1bb8491e829cc975a88f537
ameliapham <ptt.huong.8895@gmail.com>
2024-03-29 22:20:00 +0100

    Update LeftSide of Reservation

---
+++ src/pages/Reservation/LeftSide.tsx
@@ -0,0 +1,56 @@
+//import { darkTheme } from "../../theme"
+import { tss } from 'tss-react/mui'
+import { MenuBar } from '../../components/MenuBar';
+
+import backgroundImage from "../../assets/reservation2.webp"
+
+
+type PropsLeftSide = {
+    className?: string;
+    onChangePage: (page: SelectedPage) => void;
+}
+
+type SelectedPage = "home" | "menu" | "about" | "book"
+
+export function LeftSide(props: PropsLeftSide) {
+
+    const { className, onChangePage } = props
+    const { cx, classes } = useStyles()
+
+    return (
+        <div className={cx(classes.root, className)}>
+
+            <MenuBar onChangePage={onChangePage}/>
+
+            <div>
+                <h1 className={cx(classes.h1)}>
+                    Book
+                </h1>
+                <h1 className={cx(classes.h1)}>
+                    A Table
+                </h1>
+            </div>
+
+        </div >
+    )
+}
+
+const useStyles = tss
+    .create(({ theme }) => ({
+        "root": {
+            "boxSizing": "border-box",
+            "display": "flex",
+            "flexDirection": "column",
+            "justifyContent": "space-between",
+            "background": `url(${backgroundImage}) center center/cover`,
+            "borderRadius": "15px",
+            "padding": "30px 50px 30px 50px",
+        },
+        "h1": {
+            "fontFamily": theme.typography.h1.fontFamily,
+            "color": theme.palette.text.primary,
+            "fontSize": theme.typography.h1.fontSize,
+            "fontWeight": theme.typography.h1.fontWeight,
+            "margin": "0",
+        }
+    }))
