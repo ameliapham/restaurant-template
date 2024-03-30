5552d9c8d1e2112ce5e5aead8f1c57bf1043dcea
ameliapham <ptt.huong.8895@gmail.com>
2024-03-30 23:57:22 +0100

    Update RightSide of Reservation

--- src/pages/Reservation/RightSide.tsx
+++ src/pages/Reservation/RightSide.tsx
@@ -1,4 +1,5 @@
 import { tss } from 'tss-react/mui'
+import { Divider } from '@mui/material'
 
 
 type PropsRightSide = {
@@ -12,7 +13,14 @@ export function RightSide(props: PropsRightSide) {
 
     return (
         <div className={cx(classes.root, className)}>
-            
+            <div className={classes.objectHeading}>
+                <Divider className={classes.divider}>
+                    Reservation
+                </Divider>
+            </div>
+            <p className={classes.text}>
+                Book at ZenAsia for a captivating culinary adventure with Asia's finest flavors. Reserve now!
+            </p>
 
         </div>
 
@@ -27,6 +35,28 @@ const useStyles = tss
             "boxSizing": "border-box",
             "gap": "20px",
             "borderRadius": "20px",
+            "padding": "20px",
             "border": `1px solid ${theme.palette.secondary.light}`,
+        },
+        "objectHeading": {
+            "display": "flex",
+            "justifyContent": "center",
+            "alignItems": "center",
+            "fontFamily": theme.typography.fontFamily,
+            "color": theme.palette.text.primary,
+            "fontSize": theme.typography.body1.fontSize,
+        },
+        "divider": {
+            "width": "82%",
+        },
+        "text": {
+            "display": "flex",
+            "textAlign": "center",
+            "fontFamily": theme.typography.fontFamily,
+            "color": theme.palette.text.primary,
+            "fontSize": theme.typography.body2.fontSize,
+            "fontWeight": theme.typography.body2.fontWeight,
+            "margin": 0,
+            "padding": 0,
         },
     }))
