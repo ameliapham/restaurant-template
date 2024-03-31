de592c497c58dcb91bc76abc5322ed1fd1c37aa6
ameliapham <ptt.huong.8895@gmail.com>
2024-03-31 01:09:07 +0100

    Update RightSide with component InputForm

--- src/pages/Reservation/RightSide.tsx
+++ src/pages/Reservation/RightSide.tsx
@@ -1,5 +1,7 @@
 import { tss } from 'tss-react/mui'
-import { Divider } from '@mui/material'
+import Divider from '@mui/material/Divider'
+
+import { InputForm } from '../../components/InputForm'
 
 
 type PropsRightSide = {
@@ -18,9 +20,12 @@ export function RightSide(props: PropsRightSide) {
                     Reservation
                 </Divider>
             </div>
+
             <p className={classes.text}>
                 Book at ZenAsia for a captivating culinary adventure with Asia's finest flavors. Reserve now!
             </p>
+
+            <InputForm />
 
         </div>
 
@@ -33,7 +38,7 @@ const useStyles = tss
             "display": "flex",
             "flexDirection": "column",
             "boxSizing": "border-box",
-            "gap": "20px",
+            "gap": "50px",
             "borderRadius": "20px",
             "padding": "20px",
             "border": `1px solid ${theme.palette.secondary.light}`,
