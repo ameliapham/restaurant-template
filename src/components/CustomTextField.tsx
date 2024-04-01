ca5481f16f75ab03b42ccf239942127542be9f84
ameliapham <ptt.huong.8895@gmail.com>
2024-04-01 02:15:42 +0200

    Update CustomTextField

--- src/components/CustomTextField.tsx
+++ src/components/CustomTextField.tsx
@@ -1,23 +1,22 @@
 import TextField from '@mui/material/TextField'
+import { TextFieldProps } from '@mui/material/TextField';
 import { tss } from 'tss-react/mui'
 import { useState } from 'react'
 import { alpha } from '@mui/material/styles'
 
-type PropsCustomTextField = {
+type PropsCustomTextField = TextFieldProps & {
     className?: string;
-    label: string;
-    type?: string;
-    required?: boolean;
+
 }
 
 export function CustomTextField(props: PropsCustomTextField) {
 
-    const { className, label, type, required } = props
+    const { className, label, type, required = false, ...otherProps } = props
     const { cx, classes } = useStyles()
-    const [error, setError] = useState(false)
+    const [error, setError] = useState<boolean>(false)
 
     const handleValidation = (event: React.ChangeEvent<HTMLInputElement>) => {
-        if (event.target.value === "") {
+        if (required && event.target.value === "") {
             setError(true)
         } else {
             setError(false)
@@ -27,15 +26,8 @@ export function CustomTextField(props: PropsCustomTextField) {
     return (
         <TextField
             className={cx(classes.textField, className)}
-            InputLabelProps={{
-                shrink: true,
-                //className: classes.inputLabel
-            }}
-            InputProps={{
-                className: classes.input
-            }}
             label={label}
-            type = {type}
+            type={type}
             variant="standard"
             size="small"
             color="secondary"
@@ -43,17 +35,23 @@ export function CustomTextField(props: PropsCustomTextField) {
             error={error}
             helperText={error ? "This field is required" : ""}
             onChange={handleValidation}
+
+            {...otherProps} // Le reste des props au TextField
+
+            InputLabelProps={{
+                shrink: true,
+                //className: classes.inputLabel
+            }}
+
+            InputProps={{
+                className: classes.input
+            }}
         />
     )
 }
 
 const useStyles = tss
     .create(({ theme }) => ({
-        "root": {
-            "display": "flex",
-            "flexDirection": "column",
-            "gap": "20px",
-        },
         "textField": {
             "width": "100%",
             "& .MuiFilledInput-root": { // Style de l'input root
