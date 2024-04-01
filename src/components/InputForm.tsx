655cf260dc287ac3f86a1563076be131a78d9d1b
ameliapham <ptt.huong.8895@gmail.com>
2024-04-01 02:16:01 +0200

    Update InputForm

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -1,6 +1,5 @@
-import TextField from '@mui/material/TextField'
 import { tss } from 'tss-react/mui'
-import { useState } from 'react'
+import { CustomTextField } from './CustomTextField'
 
 type PropsInputForm = {
     className?: string;
@@ -10,131 +9,51 @@ export function InputForm(props: PropsInputForm) {
 
     const { className } = props
     const { cx, classes } = useStyles()
-    const [error, setError] = useState(false)
-
-    const handleValidation = (event: any) => {
-        if (event.target.value === "") {
-            setError(true)
-        } else {
-            setError(false)
-        }
-    }
 
     return (
         <div className={cx(classes.root, className)}>
-            <TextField
-                className={classes.textField}
-                InputLabelProps={{ 
-                    shrink: true,
-                    className: classes.inputLabel
-                }}
-                InputProps={{ 
-                    className: classes.input
-                }}
+            <CustomTextField
                 label="Your name"
-                variant="filled"
-                size="small"
-                color="secondary"
                 required
-                error={error}
-                helperText={error ? "This field is required" : ""}
-                onChange={handleValidation}
             />
-            <TextField
-                className={classes.textField}
-                InputLabelProps={{ shrink: true }}
+            <CustomTextField
                 label="Email"
-                variant="filled"
-                size="small"
-                color="secondary"
+                required
             />
-            <TextField
-                className={classes.textField}
-                InputLabelProps={{ shrink: true }}
+            <CustomTextField
                 label="Phone number"
-                variant="filled"
-                size="small"
-                color="secondary"
+                required
             />
-            <TextField
-                className={classes.textField}
-                InputLabelProps={{ shrink: true }}
+            <CustomTextField
                 label="Number of guests"
-                variant="filled"
-                size="small"
-                color="secondary"
+                type='number'
+                required
             />
-            <TextField
-                className={classes.textField}
-                InputLabelProps={{ shrink: true }}
+            <CustomTextField
                 label="Date"
-                variant="filled"
-                type="date"
-                size="small"
-                color="secondary"
+                type='date'
+                required
             />
-            <TextField
-                className={classes.textField}
-                InputLabelProps={{ shrink: true }}
+            <CustomTextField
                 label="Time"
-                variant="filled"
-                type="time"
-                size="small"
-                color="secondary"
+                type='time'
+                required
             />
-            <TextField
-                className={classes.textField}
-                InputLabelProps={{ shrink: true }}
+            <CustomTextField
                 label="Special requests"
-                variant="filled"
-                defaultValue={""}
                 multiline
-                rows={4}
-                size="small"
-                color="secondary"
+                rows={2}
             />
-
         </div>
 
     )
 }
 
 const useStyles = tss
-    .create(({ theme }) => ({
+    .create(({ }) => ({
         "root": {
             "display": "flex",
             "flexDirection": "column",
             "gap": "20px",
-        },
-        "textField": {
-            "width": "100%",
-            "& .MuiFilledInput-root": {
-                "background": theme.palette.secondary.light,
-                "color": theme.palette.text.primary,
-            },
-            "& .MuiFormLabel-root": {
-                "color": theme.palette.text.primary,
-            },
-            "& .MuiInputBase-input": {
-                "color": theme.palette.secondary.main,
-            },
-            "& .MuiInput-underline:before": {
-                "borderBottomColor": theme.palette.secondary.main,
-            },
-            "& .MuiInput-underline:after": {
-                "borderBottomColor": theme.palette.secondary.dark,
-            },
-            "& .MuiFormHelperText-root": {
-                "margin": "10px 0 0 0",
-                "padding": 0,
-                "lineHeight": 1,
-            },
-        },
-        "input": {
-            "fontFamily": theme.typography.fontFamily,
-
-        },
-        "inputLabel": {
-            "fontStyle": "italic",
         },
     }))
