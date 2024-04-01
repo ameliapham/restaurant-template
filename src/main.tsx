92ba9bad2d95fbe534413d87444ed1f5ded7f678
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-04-01 21:36:34 +0200

    Dark theme outside

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import ReactDOM from 'react-dom/client'
 import { App } from 'App'
 import { ThemeProvider } from "theme";
 import { FooProvider } from "tools/useFoo";
+import { DarkModeProvider } from "theme/useDarkMode";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <ThemeProvider>
-    <FooProvider fooDefaultValue="This is the default value of foo">
-      <App />
-    </FooProvider>
-  </ThemeProvider>,
+  <DarkModeProvider>
+    <ThemeProvider>
+      <FooProvider fooDefaultValue="This is the default value of foo">
+        <App />
+      </FooProvider>
+    </ThemeProvider>
+  </DarkModeProvider>
 )
--- src/theme/ThemeProvider.tsx
+++ src/theme/ThemeProvider.tsx
@@ -1,26 +1,8 @@
-import { DarkModeProvider, useDarkMode } from "./useDarkMode";
+import { useDarkMode } from "./useDarkMode";
 import { darkTheme, lightTheme } from "./theme";
 import { ThemeProvider as MuiThemeProvider } from "@mui/material/styles";
 
-type ThemeProviderProps = {
-    children: React.ReactNode;
-};
-
-export function ThemeProvider(props: ThemeProviderProps){
-
-    const { children } = props;
-
-    return (
-        <DarkModeProvider>
-            <ThemeProviderContextualized>
-                {children}
-            </ThemeProviderContextualized>
-        </DarkModeProvider>
-    );
-
-}
-
-function ThemeProviderContextualized(props: { children: React.ReactNode }){ 
+export function ThemeProvider(props: { children: React.ReactNode }){ 
 
     const { children } = props;
 
