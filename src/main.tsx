55f352e6a09ebb2f12dfea7fd9bf3f8fe5202331
ameliapham <ptt.huong.8895@gmail.com>
2024-04-03 00:13:19 +0200

    Update main

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
-import { App } from './App/App.tsx'
+import { App } from 'App'
 import { ThemeProvider } from '@mui/material/styles'
-import { darkTheme } from './theme'
+import { darkTheme } from 'theme'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <ThemeProvider theme={darkTheme}>
