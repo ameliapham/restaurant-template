c39fc6bf08d4e8788bbb0b42e1255de7f2bbfcaa
ameliapham <ptt.huong.8895@gmail.com>
2024-04-10 02:04:47 +0200

    Add NavigationButton

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -3,6 +3,11 @@ import { CustomButton } from './CustomButton'
 import logoDark from 'assets/logoDark.svg'
 import logoLight from 'assets/logoLight.svg'
 import { alpha } from '@mui/material/styles'
+import IconButton from '@mui/material/IconButton'
+import Menu from '@mui/icons-material/Menu'
+import { useState } from 'react'
+import { NavigationMenu } from './NavigationMenu'
+
 
 type PropsMenuBar = {
     className?: string;
@@ -12,31 +17,45 @@ type PropsMenuBar = {
 
 type SelectedPage = "home" | "menu" | "about" | "reservation"
 
-export function MenuBar(props: PropsMenuBar) {
+export function AppBar(props: PropsMenuBar) {
 
     const { className, onChangePage } = props
     const { cx, classes, theme } = useStyles()
+    const [openDrawer, setOpenDrawer] = useState(false)
 
     return (
         <div className={cx(classes.root, className)}>
+            <IconButton
+                className={classes.menuButton}
+                color="inherit"
+                onClick={() => setOpenDrawer(true)}
+            >
+                <Menu />
+            </IconButton>
+
+            <NavigationMenu open={openDrawer} />
+
             <img
                 className={cx(classes.logo)}
-                src = {theme.palette.mode === 'dark' ? logoDark : logoLight}
+                src={theme.palette.mode === 'dark' ? logoDark : logoLight}
                 alt="logo"
                 onClick={() => onChangePage("home")}
             />
+
             <CustomButton
                 onClick={() => onChangePage("menu")}
                 selected={props.selectedPage === "menu"}
             >
                 Menu
             </CustomButton>
+
             <CustomButton
                 onClick={() => onChangePage("about")}
                 selected={props.selectedPage === "about"}
             >
                 About
             </CustomButton>
+
             <CustomButton
                 onClick={() => onChangePage("reservation")}
                 selected={props.selectedPage === "reservation"}
@@ -54,17 +73,24 @@ const useStyles = tss
             "boxSizing": "border-box",
             "display": "inline-flex",
             "justifyContent": "center",
-            "gap": theme.spacing(1.2),
-            "padding": "5px 5px 5px 15px",
+            "gap": theme.spacing(1),
+            "padding": "5px 5px 5px 5px",
             "border": `1px solid ${theme.palette.primary.main}`,
             "borderRadius": theme.spacing(1),
             "background": theme.palette.primary.dark,
             "alignItems": "center",
             "alignSelf": "flex-start",
         },
+        "menuButton": {
+            "border": `1px solid ${theme.palette.secondary.light}`,
+            "borderRadius": "4px",
+            "padding": theme.spacing(0.6),
+
+        },
         "logo": {
             "width": theme.spacing(10),
             "cursor": "pointer",
+            "padding": "0 0 0 10px",
         },
         "reservation": {
             "border": `1px solid ${alpha(theme.palette.secondary.light, 0.5)}`,
