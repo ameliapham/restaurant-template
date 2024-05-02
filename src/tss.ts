3cffc832206f052268406f29e2badbca3599d34b
ameliapham <ptt.huong.8895@gmail.com>
2024-05-02 03:40:13 +0200

    Create custom tss

---
+++ src/tss.ts
@@ -0,0 +1,20 @@
+import { useMemo } from "react";
+import { createTss } from "tss-react";
+import { useTheme } from "@mui/material/styles";
+import { getThemeExtensions } from "theme/getThemeExtensions";
+
+function useContext() {
+
+    const theme = useTheme()
+
+    const { scrollbarStyles } = useMemo(()=> getThemeExtensions(theme), [theme])
+
+    return {
+        theme,
+        scrollbarStyles
+    }
+}
+
+export const { tss } = createTss({
+    useContext
+});
