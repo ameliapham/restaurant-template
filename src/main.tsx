adc2a6b905f0b6ef7ab2620f169cf3c1dc2441d3
ameliapham <ptt.huong.8895@gmail.com>
2024-05-03 20:58:12 +0200

    Add HeightProvider to main to make the context available to child components

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import ReactDOM from 'react-dom/client'
 import { App } from 'App'
 import { ThemeProvider } from "theme"
 import { SelectedPageProvider } from 'useSelectedPage'
+import { HeightProvider } from 'hooks/useHeightContext'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <SelectedPageProvider defaultSelectedPage="home">
     <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  </SelectedPageProvider>
+      <HeightProvider>
+        <App />
+    </HeightProvider>
+  </ThemeProvider>
+  </SelectedPageProvider >
 )
